3aef183cd0a7a54f8ab9e5eabe22d6c9d6ffd6d0
bt-ktm <your.email@example.com>
2025-06-11 14:40:27 +0545

    Extracted dataset and loaded initial CSV with basic checks

--- brain-tumor-analysis.py
+++ brain-tumor-analysis.py
@@ -1,2 +1,20 @@
-import pandas as pd 
+import zipfile
+import os
+import pandas as pd
 
+zip_path = "data/raw/archive.zip"
+extract_to = "data/raw/unzipped"
+
+os.makedirs(extract_to, exist_ok=True)
+
+
+with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+    zip_ref.extractall(extract_to)
+    
+csv_path = os.path.join(extract_to, "Brain Tumor.csv")
+df = pd.read_csv(csv_path)
+
+print(df.head())
+print(df.shape)
+print("Duplicate rows:", df.duplicated().sum())
+print(df.dtypes)
